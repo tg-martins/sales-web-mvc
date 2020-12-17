461d49a66107dbbba2530ccd5e6b6b3dad57698a
Thiago Martins <contato.thiagocesar@icloud.com>
2020-12-17 20:13:28 -0300

    other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -13,7 +13,8 @@ namespace SalesWebMvc.Data
             : base(options)
         {
         }
-
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
